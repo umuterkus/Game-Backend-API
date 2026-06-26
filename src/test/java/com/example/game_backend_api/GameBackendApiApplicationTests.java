ec85db499cf7c83748ab098a300e3f2b62126648
Umut Erkus <umuterkusofficial@gmail.com>
2026-06-26 04:08:58 +0300

    Initial commit

---
+++ src/test/java/com/example/game_backend_api/GameBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.game_backend_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
