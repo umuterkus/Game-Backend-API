2779ac9cc0defbc4d6fa9b29751ea5cc5db58895
Umut Erkus <umuterkusofficial@gmail.com>
2026-07-02 19:46:59 +0300

    Update PlayerResponse.java

--- src/main/java/com/example/game_backend_api/dto/PlayerResponse.java
+++ src/main/java/com/example/game_backend_api/dto/PlayerResponse.java
@@ -2,5 +2,16 @@ package com.example.game_backend_api.dto;
 
 public class PlayerResponse
 {
-    private final
+    private Long id ;
+    private String userName;
+    private String userEmail;
+    private String userPassword;
+
+    PlayerResponse(Long id, String userName, String userEmail, String userPassword)
+    {
+        this.id = id;
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+    }
 }
