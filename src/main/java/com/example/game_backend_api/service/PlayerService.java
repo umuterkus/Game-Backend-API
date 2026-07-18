7f7d5dcc4145e2a5d3e6a35fa65ee398ee3d7e05
Umut Erkuş <umuterkusofficial@gmail.com>
2026-07-18 13:53:17 +0300

    Logger init

--- src/main/java/com/example/game_backend_api/service/PlayerService.java
+++ src/main/java/com/example/game_backend_api/service/PlayerService.java
@@ -5,16 +5,19 @@ import com.example.game_backend_api.dto.LeaderboardEntry;
 import com.example.game_backend_api.dto.UpdatePlayerRequest;
 import com.example.game_backend_api.model.Player;
 import com.example.game_backend_api.repository.PlayerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+
 
 @Service
 public class PlayerService {
 
+    private static final Logger logger = LoggerFactory.getLogger(PlayerService.class);
     private final PlayerRepository playerRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
@@ -28,14 +31,21 @@ public class PlayerService {
     public Player createPlayer(String name, String email, String password)
     {
         if(playerRepository.existsByEmail(email))
-        { throw new IllegalArgumentException("This email already exists!"); }
+        {
+            logger.warn("This email already exists: {}", email);
+            throw new IllegalArgumentException("This email already exists!");
+        }
 
         if(playerRepository.existsByUsername(name))
-        { throw new IllegalArgumentException("This username already exists!"); }
+        {
+            logger.warn("This username already exists: {}", name);
+            throw new IllegalArgumentException("This username already exists!");
+        }
 
         Player player = new Player(name, email);
         String hashedPassword = passwordEncoder.encode(password);
         player.setPassword(hashedPassword);
+        logger.info("Player created: {}", player.getUsername());
         return playerRepository.save(player);
     }
 
